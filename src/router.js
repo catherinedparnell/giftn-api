ae1f7d0fde1f0d06c1b43697c854293b5ed6654d
Catherine Parnell <catherine.d.parnell.22@dartmouth.edu>
2020-12-18 18:22:16 -0600

    first commit

---
+++ src/router.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// your routes will go here
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import morgan from 'morgan';
+import apiRouter from './router';
+
+// initialize
+const app = express();
+
+// enable/disable cross origin resource sharing if necessary
+app.use(cors());
+
+// enable/disable http request logging
+app.use(morgan('dev'));
+
+// enable only if you want templating
+app.set('view engine', 'ejs');
+
+// enable only if you want static assets from folder static
+app.use(express.static('static'));
+
+// this just allows us to render ejs from the ../app/views directory
+app.set('views', path.join(__dirname, '../src/views'));
+
+// enable json message body for posting data to API
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+
+app.use('/api', apiRouter);
+
+// START THE SERVER
+// =============================================================================
+const port = process.env.PORT || 9090;
+app.listen(port);
+
+console.log(`listening on: ${port}`);
